6b4fdd988e7f4714ce253cca287174d0a325ba97
Stuart Morris <admin@handcode.co.uk>
2019-05-12 12:22:29 +0100

    Pulled price into product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,14 +2,18 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Product.scss";
 
-const Product = ({ product: { name } }) => {
-    return <div>{name}</div>;
+const Product = ({ product: { name, price } }) => {
+    return (
+        <div>
+            {name} - {price} - <button>Add to cart</button>
+        </div>
+    );
 };
 
 Product.propTypes = {
     product: PropTypes.shape({
-        id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
     }),
 };
 
